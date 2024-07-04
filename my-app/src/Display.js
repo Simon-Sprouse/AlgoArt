3df49a7f1260959be06f74c8697125426a125121
Simon Sprouse <simonhsprouse@gmail.com>
2024-07-04 02:22:44 -0500

    efficiency for canvas drawing

--- my-app/src/Display.js
+++ my-app/src/Display.js
@@ -11,6 +11,8 @@ function Display({ circleCount }) {
 
     useEffect( () => {
 
+        const start = new Date();
+
         const newPixels = Array.from({ length: canvasHeight }, () => 
             Array.from({ length: canvasWidth}, () => 
                 ({color: 'black'})
@@ -22,9 +24,15 @@ function Display({ circleCount }) {
         const centerY = Math.floor(Math.random() * canvasHeight);
         const radius = 20;
 
+        // Only look to change color within the square that the circle might exist in. 
+        const lowerBoundX = Math.max(0, centerX - radius);
+        const lowerBoundY = Math.max(0, centerY - radius);
+        const upperBoundX = Math.min(canvasWidth, centerX + radius);
+        const upperBoundY = Math.min(canvasHeight, centerY + radius);
+
         // can probably make this faster
-        for (let y = 0; y < canvasHeight; y++) { 
-            for (let x = 0; x < canvasWidth; x++) { 
+        for (let y = lowerBoundY; y < upperBoundY; y++) { 
+            for (let x = lowerBoundX; x < upperBoundX; x++) { 
                 const dx = x - centerX;
                 const dy = y - centerY;
                 if (dx * dx + dy * dy <= radius * radius) { 
@@ -33,21 +41,23 @@ function Display({ circleCount }) {
             }
         }
 
-        setPixels(newPixels);
+        // setPixels(newPixels); // do I actually need this state? 
 
         // draw pixels on canvas
 
         const ctx = canvasRef.current.getContext('2d');
-        pixels.forEach((row, y) => {
+        newPixels.forEach((row, y) => { // fixed an issue where the old program depended on state and was late on render
             row.forEach((pixel, x) => { 
                 ctx.fillStyle = pixel.color;
                 ctx.fillRect(x, y, 1, 1);
             });
         });
 
-        console.log("End of use effect");
-        const now = new Date();
-        console.log(now);
+        const end = new Date();
+
+        const elapsed_time = end - start;
+
+        console.log(elapsed_time);
 
 
 
@@ -56,7 +66,7 @@ function Display({ circleCount }) {
     return (
         <div>
             <p>This is the display</p>
-            <p>{circleCount}</p>
+            <p>Number of circles: {circleCount}</p>
             <canvas
                 id="canvas"
                 width={canvasWidth}
