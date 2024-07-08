caa7a7ccda0bae3b64c2d12735b47a841336b0d7
Simon Sprouse <simonhsprouse@gmail.com>
2024-07-08 12:47:45 -0500

    switch from bitmap to vector

--- my-app/src/Display.js
+++ my-app/src/Display.js
@@ -6,6 +6,17 @@ function Display({ circleCount }) {
     const canvasHeight = 400;
     const canvasWidth = 600;
 
+    /*
+    Todo list: 
+    - depreciate bitmap circle function
+    - more efficient background fill?
+    - add option for background reset
+    - fix slider initial position
+    - add slider for border size
+    - add slider for radius size
+    - add color picker wheel
+    */
+
     
     function drawCircle(array, centerX, centerY, radius, color) { 
         // Only look to change color within the square that the circle might exist in. 
@@ -26,27 +37,22 @@ function Display({ circleCount }) {
         }
     }
 
-    useEffect( () => {
+    function drawCircleByContext(ctx, centerX, centerY, radius, color) { 
+        ctx.fillStyle = color;
+        ctx.beginPath();
+        ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+        ctx.fill();
+    }
 
+
+    useEffect(() => { 
         const newPixels = Array.from({ length: canvasHeight }, () => 
             Array.from({ length: canvasWidth}, () => 
                 ({color: 'black'})
             )
         )
-
-        // generate circle
-        const centerX = Math.floor(Math.random() * canvasWidth);
-        const centerY = Math.floor(Math.random() * canvasHeight);
-        const radius = 20;
-
-        
-        drawCircle(newPixels, centerX, centerY, radius + 2, "white");
-        drawCircle(newPixels, centerX, centerY, radius, "magenta");
-
-
-        // draw pixels on canvas
-
         const ctx = canvasRef.current.getContext('2d');
+    
         newPixels.forEach((row, y) => { // fixed an issue where the old program depended on state and was late on render
             row.forEach((pixel, x) => { 
                 ctx.fillStyle = pixel.color;
@@ -54,6 +60,25 @@ function Display({ circleCount }) {
             });
         });
 
+        console.log("Starting the canvas black");
+
+    }, []);
+    
+
+    useEffect( () => {
+
+
+        // generate circle
+        const centerX = Math.floor(Math.random() * canvasWidth);
+        const centerY = Math.floor(Math.random() * canvasHeight);
+        const radius = 20;
+
+        
+        const ctx = canvasRef.current.getContext('2d');
+        drawCircleByContext(ctx, centerX, centerY, radius + 2, "white")
+        drawCircleByContext(ctx, centerX, centerY, radius, "purple");
+
+  
 
 
     }, [circleCount]) 
